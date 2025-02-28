b616f724e8d959f7816099afa66557b2383e96e2
rafa3ks <rafacarvalho231@gmail.com>
2025-02-28 00:19:05 +0000

    desafio das cartas(aventureiro)

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -14,7 +14,7 @@ int main() {
 
     char estado2;
     int codigo2;
-    char cidade2 [30];
+    char cidade2 [60];
     int populacao2;
     float area2;
     float pib2;
@@ -44,6 +44,10 @@ int main() {
     printf(" \n");
     //recolhimento de informações da primeira carta do jogo
 
+    float densidade1 = (float) populacao1 / area1;
+    float PIBCap1 = pib1 / (float) populacao1;
+    //calculo das variaveis da primeira carta
+
     printf("Carta1: \n");
     printf("Estado: %c  \n" ,estado1 );
     printf("Codigo: %c0%d  \n" ,estado1,codigo1);
@@ -52,6 +56,8 @@ int main() {
     printf("Área: %.2fkm  \n" ,area1);
     printf("PIB: %.2f Bilhões de reais  \n" ,pib1);
     printf("Número de pontos turisticos: %d  \n" ,pontost1);
+    printf("Densidade populacional: %.2f hab/km \n" ,densidade1);
+    printf("PIB per Capita: %.2f Reais \n" ,PIBCap1);
     printf(" \n");
     //print da primeira carta
 
@@ -78,6 +84,10 @@ int main() {
     printf(" \n");
     //recolhimento de informações da segunda carta do jogo
 
+    float densidade2 = (float) populacao2 / area2;
+    float PIBCap2 = pib2 / (float) populacao2;
+    //calculo das variaveis da segunda carta
+
     printf("Carta2: \n");
     printf("Estado: %c  \n" ,estado2 );
     printf("Codigo: %c0%d  \n" ,estado2,codigo2);
@@ -86,6 +96,8 @@ int main() {
     printf("Área: %.2fkm  \n" ,area2);
     printf("PIB: %.2f Bilhões de reais  \n" ,pib2);
     printf("Número de pontos turisticos: %d  \n" ,pontost2);
+    printf("Densidade populacional: %.2f hab/km \n" ,densidade2);
+    printf("PIB per Capita: %.2f Reais \n" ,PIBCap2);
     printf("  \n");
     //print da segunda carta
     
